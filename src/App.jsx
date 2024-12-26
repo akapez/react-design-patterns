84feb13352b4d81090fb234400d310a58d2559b6
Avishka <avishkakapuruge@gmail.com>
2024-12-26 10:05:01 +0530

    LE-01

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import SplitScreen from "./SplitScreen";
+
+const LeftHandComponent = ({ name }) => {
+  return <h1 style={{ backgroundColor: 'green' }}>{name}</h1>;
+};
+
+const RightHandComponent = ({ message }) => {
+  return <h1 style={{ backgroundColor: 'red' }}>{message}</h1>;
+};
 
 function App() {
   return (
-    <div>
-      <h2>React Design Patterns</h2>
-    </div>
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftHandComponent name={"Menu"} />
+      <RightHandComponent message={"Articles"} />
+    </SplitScreen>
   );
 }
 
